4db937b4f7de0c22b965ebfe27f05881a3d64599
Jaewon Jang <jewon119@gmail.com>
2021-09-15 12:34:22 +0900

    Create header and hamberger Btn

---
+++ main.js
@@ -0,0 +1,28 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul a');
+const header = document.querySelector('.header.container');
+
+// hamburger toggle Btn
+hamburger.addEventListener('click', ()=>{
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+})
+
+// scroll nav-bar in not mobile
+document.addEventListener('scroll',()=>{
+    const scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = "#29323c";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
+})
+
+// scroll nav-bar in mobile
+menu_item.forEach(item=>{
+    item.addEventListener('click',()=>{
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
+})
